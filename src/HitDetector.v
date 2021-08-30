4865a27eb8ec187fa4649fed77cc19e589bba136
kovago.zoltan <kovago.zoltan@ustream.tv>
2021-08-30 23:49:52 +0200

    Fix hit detector

--- src/HitDetector.v
+++ src/HitDetector.v
@@ -1,3 +1,5 @@
+`include "VgaTiming.v"
+
 module HitDetector(
   input  i_Clk,
   input  i_HReset,
@@ -5,16 +7,24 @@ module HitDetector(
   input  i_Ball,
   output o_XDir);
 
-  reg xdir = 0;
+  reg       xdir = 0;
+  reg [9:0] dx   = 0; // Distance from the left edge of the screen
+
+  always @(posedge i_Clk) begin
+    if (i_HReset)
+      dx <= 0;
+    if (~i_HBlank)
+      dx <= dx + 1;
+  end
 
   always @(negedge i_Clk) begin
     if (i_Ball) begin
       // Hit the left edge of the screen
-      if (xdir == 0 && i_HReset)
+      if (xdir == 0 && dx == 1)
         xdir <= 1;
 
       // Hit the right edge of the screen
-      if (xdir == 1 && i_HBlank)
+      if (xdir == 1 && dx == `H_VISIBLE_AREA)
         xdir <= 0;
     end
   end
